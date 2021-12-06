6430f1dff3b7a33f1572f5470e466f15b1844bd8
Micaela Guimaraes <micaelamarinho321@live.com>
2021-12-06 13:14:51 -0300

    first commit

---
+++ SampleJobQuartzNet/Jobs/HelloJob.cs
@@ -0,0 +1,16 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleJobQuartzNet.Jobs
+{
+    public class HelloJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await Console.Out.WriteLineAsync("Hello World - Job!");
+        }
+    }
+}
---
+++ SampleJobQuartzNet/Program.cs
@@ -0,0 +1,32 @@
+﻿using Quartz;
+using Quartz.Impl;
+using SampleJobQuartzNet.Jobs;
+using System;
+using System.Threading.Tasks;
+
+namespace SampleJobQuartzNet
+{
+    class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            StdSchedulerFactory factory = new StdSchedulerFactory();
+            IScheduler scheduler = await factory.GetScheduler();
+            await scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create<HelloJob>()
+                .WithIdentity("HelloWorld", "Hello").Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .StartNow()
+                .WithSimpleSchedule(x => x.WithIntervalInSeconds(2)
+                .WithRepeatCount(5))
+                .Build();
+
+            await scheduler.ScheduleJob(job, trigger);
+
+            await Task.Delay(TimeSpan.FromSeconds(20));
+            await scheduler.Shutdown();
+        }
+    }
+}
